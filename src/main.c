8068fd03560b5727c1b33bc22e5cd41904701996
Pete Maynard <pgm987@gmail.com>
2015-01-03 14:30:24 +0000

    Refactored for tcpdump structures

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,9 +5,53 @@
 #include <linux/types.h>
 #include <linux/netfilter.h>            /* for NF_ACCEPT */
 #include <arpa/inet.h>                  /* for inet_ntop(), inet_pton() */
-#include <string.h>                     /* for memcpy() */
+// #include <string.h>                     /* for memcpy() */
 
 #include <libnetfilter_queue/libnetfilter_queue.h>
+
+/* IP header */
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+        u_short th_sport;               /* source port */
+        u_short th_dport;               /* destination port */
+        tcp_seq th_seq;                 /* sequence number */
+        tcp_seq th_ack;                 /* acknowledgement number */
+        u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 /* window */
+        u_short th_sum;                 /* checksum */
+        u_short th_urp;                 /* urgent pointer */
+};
 
 /* returns packet id */
 static u_int32_t print_pkt (struct nfq_data *tb)
@@ -21,40 +64,85 @@ static u_int32_t print_pkt (struct nfq_data *tb)
         ph = nfq_get_msg_packet_hdr(tb);
         if (ph) {
                 id = ntohl(ph->packet_id);
-                printf("hw_protocol=0x%04x hook=%u id=%u ",
+                printf("hw_protocol=0x%04x hook=%u id=%u \n",
                         ntohs(ph->hw_protocol), ph->hook, id);
         }
 
-        // TODO: Get src/dst IP src/dest TCP port.
         // TODO: Get procs PID/Name based on meta data.
         // TODO: Check whitelist.
         ret = nfq_get_payload(tb, &data);
         if (ret >= 0)
         {
-            // Get Source IP
-            unsigned long raw_src_ip[4];
-            char src_ip[INET_ADDRSTRLEN];
+            const struct sniff_ip *ip;              /* The IP header */
+            const struct sniff_tcp *tcp;            /* The TCP header */
+            const char *payload;                    /* Packet payload */
+            
+            int size_ip;
+            int size_tcp;
+            int size_payload;
 
-            memcpy(raw_src_ip, &data[12], 4);
-            inet_ntop(AF_INET, raw_src_ip, src_ip, INET_ADDRSTRLEN);
-
-            printf("[>] SRC IP: %s\n", src_ip);
-
-            // Get Destination IP
-            unsigned long raw_dst_ip[4];
-            char dst_ip[INET_ADDRSTRLEN];
-
-            memcpy(raw_dst_ip, &data[16], 4);
-            inet_ntop(AF_INET, raw_dst_ip, dst_ip, INET_ADDRSTRLEN);
-
-            printf("[>] DST IP: %s\n", dst_ip);
-
-            /* DEBUG */
-            int i = 0;
-            printf("\n[#] Payload [%d]: ", ret);
-            for (i = 0; i < ret; i++) {
-               printf("%1X", data[i] );
+            ip = (struct sniff_ip*)(data);
+            size_ip = IP_HL(ip)*4;
+            if (size_ip < 20) {
+                printf("[!!] Invalid IP header length: %u bytes\n", size_ip);
+                return;
             }
+
+            /* print source and destination IP addresses */
+            printf("[>] From: %s\n", inet_ntoa(ip->ip_src));
+            printf("[>] To: %s\n", inet_ntoa(ip->ip_dst));
+
+            /* determine protocol */    
+            switch(ip->ip_p) {
+                case IPPROTO_TCP:
+                    printf("[>] Protocol: TCP\n");
+                    break;
+                case IPPROTO_UDP:
+                    printf("[>] Protocol: UDP\n");
+                    return;
+                case IPPROTO_ICMP:
+                    printf("[>] Protocol: ICMP\n");
+                    return;
+                case IPPROTO_IP:
+                    printf("[>] Protocol: IP\n");
+                    return;
+                default:
+                    printf("[!!] Protocol: unknown\n");
+                    return;
+            }
+
+            // Check for TCP
+            // printf("[#] Protocol: %d\n", data[9]);
+            // if( data[9] != 6)
+            //     return id; /* TODO: Handle this packet */
+
+
+            /* define/compute tcp header offset */
+            tcp = (struct sniff_tcp*)(data + size_ip);
+            size_tcp = TH_OFF(tcp)*4;
+            if (size_tcp < 20) {
+                printf("[!!] Invalid TCP header length: %u bytes\n", size_tcp);
+                return;
+            }
+            
+            printf("[>] Src port: %d\n", ntohs(tcp->th_sport));
+            printf("[>] Dst port: %d\n", ntohs(tcp->th_dport));
+
+            /* define/compute tcp payload (segment) offset */
+            payload = (u_char *)(data + size_ip + size_tcp);
+            
+            /* compute tcp payload (segment) size */
+            size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+
+            if (size_payload > 0)
+                printf("   Payload (%d bytes):\n", size_payload);
+    
+            /* DEBUG */
+            // int i = 0;
+            // printf("\n[#] Payload [%d]: ", ret);
+            // for (i = 0; i < ret; i++) {
+            //    printf("%1X", data[i] );
+            // }
         }
 
         fputc('\n', stdout);
