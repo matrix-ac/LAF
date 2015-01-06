2518c014962bc4f51e4a12f7d367988ccca17dd2
Pete Maynard <pgm987@gmail.com>
2015-01-06 18:50:30 +0000

    GPLv3 License

--- src/procs.c
+++ src/procs.c
@@ -1,3 +1,20 @@
+/*
+    This file is part of Linux Application Firewall (LAF).
+
+    Linux Application Firewall (LAF) is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Linux Application Firewall (LAF) is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Linux Application Firewall (LAF).  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "procs.h"
 
 /* Takes IP_SRC SRC_PORT, IP_DST DST_PORT and returns a string of the associated binary name with the socket. */
